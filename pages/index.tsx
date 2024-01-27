97ee71b267b14a958125f2a8380b6b83866816a3
Yumo Zhou <113385234+NAinZhou@users.noreply.github.com>
2024-01-26 18:33:37 -0800

    Set up the project structure
    Set up the project structure and add content to the navbar and main section components

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import NavigationBar from "../components/NavigationBar";
+import MainSection from "../components/MainSection";
+// import VHRSection from "../components/VHR";
+// import DiscussionSection from "../components/Discussion";
+// import MapSection from "../components/Map";
+// import SourceSection from "../components/GoToSource";
+// import Footer from "../components/Footer";
+
+const HomePage = () => {
+  return (
+    <>
+      <NavigationBar />
+      <MainSection />
+      {/* <VHR />
+      <Discussion />
+      <Map />
+      <Source />
+      <Footer /> */}
+    </>
+  );
+};
+
+export default HomePage;
