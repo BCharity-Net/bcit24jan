b45533ccf4256802e34e484e8469abc66612ff6f
Lucas <113465142+LucasHYing@users.noreply.github.com>
2024-01-31 23:23:32 -0800

    added footer and discussion cards for more interesting discussion section

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,29 +6,28 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import NavigationBar from "../components/NavigationBar";
 import MainSection from "../components/MainSection";
-// import VHRSection from "../components/VHR";
+import VHRSection from "../components/VHR";
 import DiscussionSection from "../components/Discussion";
 // import MapSection from "../components/Map";
 // import SourceSection from "../components/GoToSource";
-// import Footer from "../components/Footer";
+import Footer from "../components/Footer";
 
 const HomePage = () => {
   return (
     <>
       <NavigationBar />
       <MainSection />
-      {
-        //<VHR />
-        <DiscussionSection />
-        /*
-      <Map />
-      <Source />
-      <Footer /> 
-      */
+      <DiscussionSection />
+      <VHRSection />
+      {/*
+        <Map />
+        <Source />*/
+        <Footer /> 
       }
     </>
   );
 };
+
 
 export async function getStaticProps({ locale }: GetStaticPropsContext) {
   return {
