12642873e66c26ef0339ad06f9b1a67dcdaf6d6d
VietNguyenSet1D <vnguyen141@my.bcit.ca>
2024-02-01 00:56:15 -0800

    Added Go To Resource section

---
+++ components/DiscussionCard.tsx
@@ -0,0 +1,45 @@
+// DiscussionSection component
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { FiArrowRightCircle } from "react-icons/fi";
+import { useTranslation } from 'next-i18next';
+import '../app/globals.css';
+
+
+interface DiscussionCardProps {
+  imageSrc: string;
+  title: string;
+  numberOfComments: number;
+}
+
+const DiscussionCard: React.FC<DiscussionCardProps> = ({ imageSrc, title, numberOfComments }) => {
+  const { t } = useTranslation('common');
+
+  const titleId = `title-${title.replace(/\s+/g, '-')}`; // replace spaces with dashes for ARIA functionality
+  
+  return (
+    <div className="discussion-card max-w-sm rounded overflow-hidden shadow-lg">
+      <div className="relative">
+        <Image src={imageSrc} alt={title} width={1000} height={667} className="w-full h-full object-cover rounded" />
+        <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-end">
+          <div className="px-6 py-4 bg-white bg-opacity-0">
+            <span className="inline-block bg-purple-700 rounded-full px-3 py-1 text-sm font-semibold text-white mb-2 max-w-full">
+              {t('comment', { count: numberOfComments })}
+            </span>
+            <div className="flex items-center">
+              <div id={titleId} className=" text-xl mb-2 text-white max-w-full">{title}</div>
+              <div className="ml-auto">
+                <Link href={`/discussion/${title}`} aria-labelledby={titleId}>
+                    <FiArrowRightCircle size={30} color="grey" />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DiscussionCard;
---
+++ components/MainSection.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import Image from "next/image";
+import { useTranslation } from 'next-i18next';
+
+const MainSection = () => {
+  const { t } = useTranslation(['common', 'home']);
+  return (
+    <section
+      className="relative flex items-center justify-between text-white mt-10 mb-20 max-w-screen-xl mx-auto"
+      style={{ backgroundColor: "transparent" }}
+    >
+      <div className="z-10 w-full md:w-1/3">
+        <h1 className="text-7xl font-bold">
+          {/* Page Title */}
+          {t('home:home-page-title')}
+        </h1>
+        <p className="mt-4 mb-6">
+          {/* Page Subtitle */}
+          {t('home:home-page-subtitle')}
+        </p>
+        <div className="flex flex-col md:flex-row">
+          <Link
+            href="/login"
+            className="inline-block bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 mr-2 mb-2 md:mb-0"
+          >
+            {/* Login Button */}
+            {t('common:account-login')}
+          </Link>
+          <Link
+            href="/discussions"
+            className="inline-block bg-transparent border-2 border-white text-white py-2 px-4 rounded hover:bg-white hover:text-black"
+          >
+            {/* Discussions Button */}
+            {t('common:discussions')}
+          </Link>
+        </div>
+      </div>
+      <div className="hidden md:block md:absolute md:inset-y-0 md:right-0 md:w-1/3">
+        <Image
+          src="/bg-hero.png"
+          alt="Background Image"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default MainSection;
