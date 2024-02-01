642989b67e2548d2f5d7c8da1493feed309fcc05
Lucas <113465142+LucasHYing@users.noreply.github.com>
2024-02-01 12:59:51 -0800

    deleted unnecessary comments

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import NavigationBar from "../components/NavigationBar";
 import MainSection from "../components/MainSection";
-import VHRSection from "../components/VHR";
+//import VHRSection from "../components/VHR";
 import DiscussionSection from "../components/Discussion";
 // import MapSection from "../components/Map";
 // import SourceSection from "../components/GoToSource";
@@ -18,7 +18,6 @@ const HomePage = () => {
       <NavigationBar />
       <MainSection />
       <DiscussionSection />
-      <VHRSection />
       {/*
         <Map />
         <Source />*/
