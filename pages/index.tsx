4647ace82712910bee4f6cbafa9ff6a47830bfa7
Yumo Zhou <113385234+NAinZhou@users.noreply.github.com>
2024-02-15 12:10:55 -0800

    fff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "../app/globals.css";
 
-import { GetStaticPropsContext } from 'next';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { GetStaticPropsContext } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 import NavigationBar from "../components/NavigationBar";
 import MainSection from "../components/MainSection";
 //import VHRSection from "../components/VHR";
 import DiscussionSection from "../components/Discussion";
-import AltDiscussionSection from "../components/AltDiscussion"
+import AltDiscussionSection from "../components/AltDiscussion";
 // import MapSection from "../components/Map";
 // import SourceSection from "../components/GoToSource";
 import Footer from "../components/Footer";
@@ -20,18 +20,21 @@ const HomePage = () => {
       <NavigationBar />
       <MainSection />
       <DiscussionSection />
-      <AltDiscussionSection/>
-      <SourceSection/>
-      <Footer /> 
+      <AltDiscussionSection />
+      <SourceSection />
+      <Footer />
     </>
   );
 };
 
-
 export async function getStaticProps({ locale }: GetStaticPropsContext) {
   return {
     props: {
-      ...(await serverSideTranslations(locale ?? 'en', ['navbar', 'common', 'home'])),
+      ...(await serverSideTranslations(locale ?? "en", [
+        "navbar",
+        "common",
+        "home",
+      ])),
     },
   };
 }
