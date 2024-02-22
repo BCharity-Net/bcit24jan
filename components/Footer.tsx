313d99e6717963b7a4860ffbd86f62b1f0fd3066
Lucas <113465142+LucasHYing@users.noreply.github.com>
2024-02-22 12:09:32 -0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
-// Footer.tsx
 import React from 'react';
 import { useTranslation } from 'next-i18next';
-// Import icons here, if using specific icon components
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faYoutube, faTwitter, faInstagram, faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Footer: React.FC = () => {
   const { t } = useTranslation();
@@ -11,19 +11,17 @@ const Footer: React.FC = () => {
         <hr className="border-gray-800" />
         <br></br>
         <br></br>
-      <div className="container mx-auto flex flex-col lg:flex-row justify-between items-center">
+        <div className="container mx-auto max-w-6xl flex flex-col lg:flex-row justify-center items-center lg:justify-between">
         <div className="flex flex-col items-center lg:items-start mb-4 lg:mb-0">
-          {/* Replace src with actual logo image */}
           <img src="/logo.png" alt="Logo" className="w-12 h-12 mb-2" />
           <span className="mb-2">{t('managedBy')}</span>
-          {/* Social Media icons */}
           <div className="flex">
-            {/* Replace these placeholders with actual icon components */}
-            <a href="#" className="mr-2">YT</a>
-            <a href="#" className="mr-2">TW</a>
-            <a href="#" className="mr-2">IG</a>
-            <a href="#" className="mr-2">FB</a>
-            <a href="#" className="mr-2">LI</a>
+            {/* Social Media icons */}
+            <a href="#" className="mr-2"><FontAwesomeIcon icon={faYoutube} /></a>
+            <a href="#" className="mr-2"><FontAwesomeIcon icon={faTwitter} /></a>
+            <a href="#" className="mr-2"><FontAwesomeIcon icon={faInstagram} /></a>
+            <a href="#" className="mr-2"><FontAwesomeIcon icon={faFacebook} /></a>
+            <a href="#" className="mr-2"><FontAwesomeIcon icon={faLinkedin} /></a>
           </div>
           <div>
             <p className="text-gray-500 hover:text-white">© 2023 BCharity Baas. All rights reserved.</p>
@@ -31,12 +29,12 @@ const Footer: React.FC = () => {
         </div>
 
         <div className="lg:flex lg:gap-12">
-          {/* BCharity column */}
+          {/* SOLANA column */}
           <div className="mb-4 lg:mb-0">
-            <h6 className="font-bold mb-1">{t('BCharity')}</h6>
+            <h6 className="font-bold mb-1">{t('SOLANA')}</h6>
             <ul>
                 <li><a href="#" className="text-gray-500 hover:text-white">{t('VHRs')}</a></li>
-                <li><a href="#" className="text-gray-500 hover:text-white">{t('Break BCharity')}</a></li>
+                <li><a href="#" className="text-gray-500 hover:text-white">{t('Break Solana')}</a></li>
                 <li><a href="#" className="text-gray-500 hover:text-white">{t('Groups')}</a></li>
                 <li><a href="#" className="text-gray-500 hover:text-white">{t('Careers')}</a></li>
                 <li><a href="#" className="text-gray-500 hover:text-white">{t('Disclaimer')}</a></li>
@@ -52,7 +50,7 @@ const Footer: React.FC = () => {
               <li><a href="#" className="text-gray-500 hover:text-white">{t('Newsletter')}</a></li>
             </ul>
           </div>
-        
+        </div>
 
         <div className="flex flex-col items-center lg:items-end">
           <div className="mb-2">
@@ -64,7 +62,6 @@ const Footer: React.FC = () => {
           </div>
         </div>
         </div>
-      </div>
     </footer>
   );
 };
