a84942911f666d196c50029e5025f71ebaf3c290
VietNguyenSet1D <vnguyen141@my.bcit.ca>
2024-04-03 19:55:45 -0700

    commit

--- components/LogoBanner.tsx
+++ components/LogoBanner.tsx
@@ -11,19 +11,19 @@ export default function LogoBanner() {
                         <div className='logos-slide'>
                             <img src="/canada_gov.png" alt="" /> 
                             <img src="/Ecssen_logo.png" alt="" />
-                            <img src="/SignalHire_logo.png " alt="" />
+                            <img src="/SignalHire_logo_logo.png " alt="" />
                             <img src="/UBC_logo.png" alt="" />
                             <img src="/BCIT_logo.png" alt="" />
-                            <img src="/Founder_logo.png" alt="" />
+                            <img src="/Founder_logo_copy.png" alt="" />
                         </div>
 
                         <div className='logos-slide'>
-                            <img src="/canada_gov.png" alt="" /> 
                             <img src="/Ecssen_logo.png" alt="" />
-                            <img src="/SignalHire_logo.png " alt="" />
+                            <img src="/canada_gov.png" alt="" /> 
+                            <img src="/SignalHire_logo_logo.png " alt="" />
                             <img src="/UBC_logo.png" alt="" />
                             <img src="/BCIT_logo.png" alt="" />
-                            <img src="/Founder_logo.png" alt="" />
+                            <img src="/Founder_logo_copy.png" alt="" />
                         </div>
                     </div>
         //         </div>    
