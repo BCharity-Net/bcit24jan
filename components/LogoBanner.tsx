50e043130b344ce1a24e594d52a9e798c11cffce
VietNguyenSet1D <vnguyen141@my.bcit.ca>
2024-04-11 14:02:17 -0700

    minimodify

--- components/LogoBanner.tsx
+++ components/LogoBanner.tsx
@@ -1,33 +1,26 @@
 import '../app/globals.css';
 import '../app/LogoBanner.css';
 
-
 export default function LogoBanner() {
     return (
-        // <main className="relative overflow-hidden"> 
-        //     <div className='max-w-screen mx-auto mb-10'> 
-        //         <div className='max-w-screen-xl mx-auto'>
-                    <div className='logos'>
-                        <div className='logos-slide'>
-                            <img src="/canada_gov.png" alt="" /> 
-                            <img src="/Ecssen_logo.png" alt="" />
-                            <img src="/SignalHire_logo_logo.png " alt="" />
-                            <img src="/UBC_logo.png" alt="" />
-                            <img src="/BCIT_logo.png" alt="" />
-                            <img src="/Founder_logo_copy.png" alt="" />
-                        </div>
+        <div className='logos'>
+            <div className='logos-slide'>
+                <img src="/canada_gov.png" alt="" /> 
+                <img src="/Ecssen_logo.png" alt="" />
+                <img src="/SignalHire_logo_logo.png " alt="" />
+                <img src="/UBC_logo.png" alt="" />
+                <img src="/BCIT_logo.png" alt="" />
+                <img src="/Founder_logo_copy.png" alt="" />
+            </div>
 
-                        <div className='logos-slide'>
-                            <img src="/canada_gov.png" alt="" /> 
-                            <img src="/Ecssen_logo.png" alt="" />
-                            <img src="/SignalHire_logo_logo.png " alt="" />
-                            <img src="/UBC_logo.png" alt="" />
-                            <img src="/BCIT_logo.png" alt="" />
-                            <img src="/Founder_logo_copy.png" alt="" />
-                        </div>
-                    </div>
-        //         </div>    
-        //     </div>
-        // </main>
+            <div className='logos-slide'>
+                <img src="/canada_gov.png" alt="" /> 
+                <img src="/Ecssen_logo.png" alt="" />
+                <img src="/SignalHire_logo_logo.png " alt="" />
+                <img src="/UBC_logo.png" alt="" />
+                <img src="/BCIT_logo.png" alt="" />
+                <img src="/Founder_logo_copy.png" alt="" />
+            </div>
+        </div>
     );
 }
